a9fe7b9794b5f621f3e70416824df1afc58ee939
therealpwnguy1 <49798816+therealpwnguy1@users.noreply.github.com>
2019-08-08 11:09:59 -0400

    Added actual code

---
+++ src/main/java/me/therealpwnguy1/recipemod/RecipeMod.java
@@ -0,0 +1,12 @@
+package me.therealpwnguy1.recipemod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class RecipeMod implements ModInitializer 
+{
+	@Override
+	public void onInitialize() 
+	{
+		
+	}
+}
